c30274f7d12c173e1dc89d4d2b684bfb72464238
Daniel <arevalodev20@gmail.com>
2020-08-28 02:20:05 -0600

    Agregando web con error de registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {appRoutingProviders, routing} from './app.routing';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FotografiaComponent } from './components/fotografia/fotografia.component';
+import { VideoComponent } from './components/video/video.component';
+import { LocucionComponent } from './components/locucion/locucion.component';
+import { DesignComponent } from './components/design/design.component';
+import { DesarrolloWebComponent } from './components/desarrollo-web/desarrollo-web.component';
+import { ManagerComponent } from './components/manager/manager.component';
+import { ModelajeComponent } from './components/modelaje/modelaje.component';
+import { MaquillajeComponent } from './components/maquillaje/maquillaje.component';
+import { ErrorComponent } from './components/error/error.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FotografiaComponent,
+    VideoComponent,
+    LocucionComponent,
+    DesignComponent,
+    DesarrolloWebComponent,
+    ManagerComponent,
+    ModelajeComponent,
+    MaquillajeComponent,
+    ErrorComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
